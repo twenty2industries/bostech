008471720f5491168c04154a64f04704ba5c62ce
twenty2industries <burak.bostanli.1@web.de>
2025-11-01 19:43:26 +0100

    Add hero section and Poppins font integration
    Introduced a new HeroSectionComponent with HTML, SCSS, and test files, and added it to the main app layout. Updated header navigation and styles, added a background image, and integrated the Poppins font family with multiple weights. Global styles and header appearance were improved for a more modern look.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
