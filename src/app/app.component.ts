2f2be4408bd4e4d8bc5bc5b424c1fa1807deba09
twenty2industries <burak.bostanli.1@web.de>
2025-11-02 20:00:22 +0100

    Add services section and update header logo
    Introduced a new ServicesSectionComponent with template, styles, and tests, and integrated it into the main app component. Replaced the header text logo with an image, updated header and burger menu styles, and made minor cleanup in the projects section.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
+import { ServicesSectionComponent } from './components/services-section/services-section.component';
+
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroSectionComponent, ProjectsSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroSectionComponent, ProjectsSectionComponent, ServicesSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
