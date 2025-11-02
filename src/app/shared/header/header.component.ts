e0c8924a9187001d7fd58a5da23aaef586b29214
Burak B. <burak.bostanli.1@web.de>
2025-11-02 18:56:31 +0100

    Add responsive burger menu to header
    Introduces a burger menu for the header component to improve navigation on smaller screens. Updates HTML, SCSS, and TypeScript to support toggling the menu, adjusts header and nav styles, and modifies hero section height for better layout.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  menuOpen: boolean = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    console.log(this.menuOpen);
+  }
 }
