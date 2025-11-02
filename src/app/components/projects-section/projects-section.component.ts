dfd2d87877b2b8e5e5669cbcc93a3ac319f0298f
Burak B. <burak.bostanli.1@web.de>
2025-11-02 11:31:57 +0100

    Add Overtake project and update project section UI
    Added the Overtake project to the projects list with corresponding image and details. Updated the project image rendering to use dynamic filenames based on project title. Improved project section styles with new backgrounds, padding, and layout adjustments. Removed unused image and updated global font to Poppins.

--- src/app/components/projects-section/projects-section.component.ts
+++ src/app/components/projects-section/projects-section.component.ts
@@ -9,15 +9,25 @@ import { Project } from '../../shared/interfaces/interface';
 })
 export class ProjectsSectionComponent {
 
-//#region properties
-project: Project[] = [{
-title: 'Join',
-duration: 'Drei Wochen',
-imageUrl: 'assets/images/projects/join.png',
-description: 'Ein Kanban-Manager mit Dashboard, Task-Manager und Kontaktverwaltung. Nutzer können Tasks mit Prioritäten erstellen, filtern und im Board verwalten. Das System bietet volle CRUD-Funktionen für Tasks und Kontakte.',
-techstack: ['Angular', 'Firebase'],
-projectUrl: ''
-}]
-//#endregion properties
-  
+  //#region properties
+  project: Project[] = [{
+    title: 'Join',
+    duration: 'Drei Wochen',
+    imageUrl: 'assets/images/projects/Join.png',
+    description: 'Ein Kanban-Manager mit Dashboard, Task-Manager und Kontaktverwaltung. Nutzer können Tasks mit Prioritäten erstellen, filtern und im Board verwalten. Das System bietet volle CRUD-Funktionen für Tasks und Kontakte.',
+    techstack: ['Angular', 'Firebase'],
+    projectUrl: ''
+  },
+  {
+    title: 'Overtake',
+    duration: 'Sieben Wochen',
+    imageUrl: 'assets/images/projects/Overtake.png',
+    description: 'Eine Angular-App zur Darstellung von Fahrer- und Konstrukteurswertungen mit Fullcards für detaillierte Fahrerinfos. Geplant sind Features wie Rennkalender, Sessions und Live-Daten.',
+    techstack: ['Angular', 'Firebase', 'REST API'],
+    projectUrl: ''
+  }
+
+  ]
+  //#endregion properties
+
 }
