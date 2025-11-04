b9bf5c73c2db2d1a0122d44fa41eda68eaf9db0a
twenty2industries <burak.bostanli.1@web.de>
2025-11-04 11:15:20 +0100

    Add AboutSection component and integrate into app
    Introduced a new AboutSection component with its template, styles, and test. Updated app.component to include and display the AboutSection. Also made minor style adjustments to the ServicesSection component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from './shared/header/header.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
 import { ServicesSectionComponent } from './components/services-section/services-section.component';
+import { AboutSectionComponent } from './components/about-section/about-section.component';
 
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroSectionComponent, ProjectsSectionComponent, ServicesSectionComponent],
+  imports: [RouterOutlet, AboutSectionComponent, HeaderComponent, HeroSectionComponent, ProjectsSectionComponent, ServicesSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/about-section/about-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-section',
+  imports: [],
+  templateUrl: './about-section.component.html',
+  styleUrl: './about-section.component.scss'
+})
+export class AboutSectionComponent {
+
+}
